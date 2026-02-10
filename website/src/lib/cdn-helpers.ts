dea2cc08dd68e6ac63202b3ad0e89487e056027c
Laurentiu Cotet <cotetlaurentiu@gmail.com>
2026-02-10 12:44:24 +0200

    Update CDN version to @identitate-ro/logos@1.3.0 for website

--- website/src/lib/cdn-helpers.ts
+++ website/src/lib/cdn-helpers.ts
@@ -9,7 +9,7 @@ import type { AssetUrls } from '../types/institution';
 /**
  * Versiunea pachetului npm pentru CDN URLs
  */
-export const CDN_VERSION = '1.2.2';
+export const CDN_VERSION = '1.3.0';
 
 /**
  * Pattern-uri CDN
